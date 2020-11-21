0651aa05a64b6c852850f055422671f72c43ebf6
zhangweidong <doter1995@qq.com>
2020-11-22 01:43:21 +0800

    init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/doter1995/user_center
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,10 @@
+package user_center
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	startService()
+}
+func startService() {
+	_ = gin.Default()
+}
